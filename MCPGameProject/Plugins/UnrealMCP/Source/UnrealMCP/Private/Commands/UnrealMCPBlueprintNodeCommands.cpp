dda418f6628cd01823915fc0efd8e5671295551d
Chong-U Lim <me@chongdashu.com>
2025-04-03 15:19:33 +0100

    refactor: Remove create input mapping command handler from UnrealMCPBlueprintNodeCommands

--- MCPGameProject/Plugins/UnrealMCP/Source/UnrealMCP/Private/Commands/UnrealMCPBlueprintNodeCommands.cpp
+++ MCPGameProject/Plugins/UnrealMCP/Source/UnrealMCP/Private/Commands/UnrealMCPBlueprintNodeCommands.cpp
@@ -30,10 +30,6 @@ TSharedPtr<FJsonObject> FUnrealMCPBlueprintNodeCommands::HandleCommand(const FSt
     {
         return HandleConnectBlueprintNodes(Params);
     }
-    else if (CommandType == TEXT("create_input_mapping"))
-    {
-        return HandleCreateInputMapping(Params);
-    }
     else if (CommandType == TEXT("add_blueprint_get_self_component_reference"))
     {
         return HandleAddBlueprintGetSelfComponentReference(Params);
@@ -146,61 +142,6 @@ TSharedPtr<FJsonObject> FUnrealMCPBlueprintNodeCommands::HandleConnectBlueprintN
     }
 
     return FUnrealMCPCommonUtils::CreateErrorResponse(TEXT("Failed to connect nodes"));
-}
-
-TSharedPtr<FJsonObject> FUnrealMCPBlueprintNodeCommands::HandleCreateInputMapping(const TSharedPtr<FJsonObject>& Params)
-{
-    // Get required parameters
-    FString ActionName;
-    if (!Params->TryGetStringField(TEXT("action_name"), ActionName))
-    {
-        return FUnrealMCPCommonUtils::CreateErrorResponse(TEXT("Missing 'action_name' parameter"));
-    }
-
-    FString Key;
-    if (!Params->TryGetStringField(TEXT("key"), Key))
-    {
-        return FUnrealMCPCommonUtils::CreateErrorResponse(TEXT("Missing 'key' parameter"));
-    }
-
-    // Get the input settings
-    UInputSettings* InputSettings = GetMutableDefault<UInputSettings>();
-    if (!InputSettings)
-    {
-        return FUnrealMCPCommonUtils::CreateErrorResponse(TEXT("Failed to get input settings"));
-    }
-
-    // Create the input action mapping
-    FInputActionKeyMapping ActionMapping;
-    ActionMapping.ActionName = FName(*ActionName);
-    ActionMapping.Key = FKey(*Key);
-
-    // Add modifiers if provided
-    if (Params->HasField(TEXT("shift")))
-    {
-        ActionMapping.bShift = Params->GetBoolField(TEXT("shift"));
-    }
-    if (Params->HasField(TEXT("ctrl")))
-    {
-        ActionMapping.bCtrl = Params->GetBoolField(TEXT("ctrl"));
-    }
-    if (Params->HasField(TEXT("alt")))
-    {
-        ActionMapping.bAlt = Params->GetBoolField(TEXT("alt"));
-    }
-    if (Params->HasField(TEXT("cmd")))
-    {
-        ActionMapping.bCmd = Params->GetBoolField(TEXT("cmd"));
-    }
-
-    // Add the mapping
-    InputSettings->AddActionMapping(ActionMapping);
-    InputSettings->SaveConfig();
-
-    TSharedPtr<FJsonObject> ResultObj = MakeShared<FJsonObject>();
-    ResultObj->SetStringField(TEXT("action_name"), ActionName);
-    ResultObj->SetStringField(TEXT("key"), Key);
-    return ResultObj;
 }
 
 TSharedPtr<FJsonObject> FUnrealMCPBlueprintNodeCommands::HandleAddBlueprintGetSelfComponentReference(const TSharedPtr<FJsonObject>& Params)
